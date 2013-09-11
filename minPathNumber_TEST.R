9e3c1e05781cfda231169376071e5faffa49ce40
Dave Gerrard <davetgerrard@gmail.com>
2013-09-11 13:34:05 +0100

    added more examples showing performance tests

--- minPathNumber_TEST.R
+++ minPathNumber_TEST.R
@@ -15,3 +15,38 @@ minPathNumber(my.juncs)		# should be 3
 minPathNumber(my.juncs.complex)		# should be 4 
 
 
+
+### N.B. Works for small numbers of overlapping junctions (<10) but quickly becomes very slow to compute for anything more.
+
+# Another test set with more junctions and overlaps.
+testJuncs <- data.frame(chr="chr1", start=seq(1, 801, by=50), end= seq(121, 921, by=50))
+
+
+# Don't do this it takes a VERY LONG TIME
+# system.time(minPathNumber(testJuncs))
+
+
+#instead, plot the times for 1:12 juncs
+times <- numeric()
+
+for(i in 1:12)  {
+	times[i] <- system.time(minPathNumber(testJuncs[1:i,]))['elapsed']
+
+}
+
+plot(times, xlab="number of junctions", ylab="time")
+
+
+# any kind of approximation for splcing complexity?  
+# Number of overlaps not inlcuding self:- 
+sum(getOverlapMatrix(testJuncs$start, testJuncs$end, half_matrix=TRUE), na.rm=T) - nrow(testJuncs)
+
+# but can easily show that this number is not a good predictor of solvability with minPathNumber()
+# e.g. compare the above testJuncs (which are very slow to compute) with a set where no junctions overlap EXCEPT two that overlap all others.
+testJuncs.easy <- data.frame(chr="chr1", start=c(2,3, seq(1, 751, by=50)), end= c(820,821,seq(21, 771, by=50)))
+sum(getOverlapMatrix(testJuncs.easy $start, testJuncs.easy $end, half_matrix=TRUE), na.rm=T) - nrow(testJuncs.easy )
+
+#system.time(minPathNumber(testJuncs.easy))
+# Errrr. not easy apparently!  This one also seems to take a very long time (i stopped it after 15 mins). 
+
+##   GRRRR NP-COMPLETE!  http://en.wikipedia.org/wiki/Set_cover_problem
