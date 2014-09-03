9264ed41513bcbcf2815c64506779498a71086e3
Dave Gerrard <davetgerrard@gmail.com>
2014-09-03 16:36:59 +0100

    add inv.tau

--- calcTau.R
+++ calcTau.R
@@ -4,7 +4,7 @@
 
 
 testProfile <- c(0,8,0,0,0,2,0,2,0,0,0,0)  # tau = 0.95
-
+testProfile2 <- c(0.8, 0.8, 0.8, 0.1, 0.8, 0.8, 0.8)  # tau = 0.14  inv.tau= 0.87
 
 ## Tau from Yanai et al. (2005) Bioinformatics.
 tau <- function(profile)  {
@@ -12,14 +12,25 @@ tau <- function(profile)  {
 	
 } 
 
+inv.tau <- function(profile)  {
+    (sum(1 - (min(profile)/profile))) / (length(profile) -1 )
+    
+}
+
+
+
 
 (tau(testProfile) )
 
-testProfile <- rep(1,20)
+
+(tau(testProfile2) )
+(inv.tau(testProfile2) )
+
+#testProfile <- rep(1,20)
 
 # testProfile <- rep(0,20)
 
 
 
-testProfile <- c(0,2,0,0,0,2,0,2,0,0,0,0)
+#testProfile <- c(0,2,0,0,0,2,0,2,0,0,0,0)
 
