03f857557c34a614986d30570a2b5bdfb15dbb2e
Dave Gerrard <davetgerrard@gmail.com>
2016-02-24 15:02:09 +0000

    adding showIntenseScale, repurpose showMaxScale

--- maxScaledHeatmap.R
+++ maxScaledHeatmap.R
@@ -3,8 +3,9 @@
 
 
 
-maxScaledHeatmap <- function(df, minMax=0, showMaxScale=FALSE, showLegend=TRUE, useLog10scale=FALSE,  x.adjust = .020 ,
-                             legend.title="intensity", mar.vec =c(10,10,10,10), name.sides=1:4, scale.colour="forestgreen")  {
+maxScaledHeatmap <- function(df, minMax=0, showMaxScale=FALSE, maxScale.ints=1:5, showIntenseScale=TRUE, showLegend=TRUE, useLog10scale=FALSE,  x.adjust = .020 ,
+                             legend.title="intensity", mar.vec =c(10,10,10,10), name.sides=1:4, scale.colour="forestgreen", 
+                             font.row=1,font.col=1, cex.row =1, cex.col=1, row.labs=row.names(df), col.labs=colnames(df), showGrid=TRUE)  {
   
   max.vec <- apply(df, 1, max)
   exclude.index <- max.vec < minMax
@@ -14,7 +15,7 @@ maxScaledHeatmap <- function(df, minMax=0, showMaxScale=FALSE, showLegend=TRUE, 
   if(useLog10scale)  max.vec <- log10(max.vec)
   
   if(nrow(df) > 0 ) {
-    if(showMaxScale)  {
+    if(showIntenseScale)  {
       if(useLog10scale) {
         legend.ints <- 1:ceiling(max(max.vec))
       } else {
@@ -33,12 +34,12 @@ maxScaledHeatmap <- function(df, minMax=0, showMaxScale=FALSE, showLegend=TRUE, 
      
       x.corners <- seq(0-x.adjust,1 + x.adjust, length.out=ncol(df)+1)
       y.corners <- seq(0-y.adjust,1 + y.adjust, length.out=nrow(df)+1)
-      rect(xleft=rep(x.corners[1:ncol(df)], each=nrow(df)), ybottom=rep(y.corners[1:nrow(df)], times=ncol(df)), xright=rep(x.corners[1:(ncol(df)+1)], each=nrow(df)), ytop=rep(y.corners[2:(nrow(df)+1)], times=ncol(df)), lty=3, lwd=1, border="grey")
-      rect(xleft=-.2, ybottom=y.corners[1:nrow(df)], xright=-.1, ytop=y.corners[2:(nrow(df)+1)], lty=3, border="grey")
-      if(4 %in% name.sides) mtext(row.names(df), side=4, at=seq(0,1,length.out=nrow(df)), las=2, line=1)
-      if(1 %in% name.sides) mtext(colnames(df), side=1, at=seq(0,1,length.out=ncol(df)), las=2, line=1)  
-      if(2 %in% name.sides) mtext(row.names(df), side=2, at=seq(0,1,length.out=nrow(df)), las=2, line=1)
-      if(3 %in% name.sides) mtext(colnames(df), side=3, at=seq(0,1,length.out=ncol(df)), las=2, line=1)  
+      if(showGrid) rect(xleft=rep(x.corners[1:ncol(df)], each=nrow(df)), ybottom=rep(y.corners[1:nrow(df)], times=ncol(df)), xright=rep(x.corners[1:(ncol(df)+1)], each=nrow(df)), ytop=rep(y.corners[2:(nrow(df)+1)], times=ncol(df)), lty=3, lwd=1, border="grey")
+      if(showGrid) rect(xleft=-.2, ybottom=y.corners[1:nrow(df)], xright=-.1, ytop=y.corners[2:(nrow(df)+1)], lty=3, border="grey")
+      if(4 %in% name.sides) mtext(row.labs, side=4, at=seq(0,1,length.out=nrow(df)), las=2, line=1, font=font.row, cex=cex.row)
+      if(1 %in% name.sides) mtext(col.labs, side=1, at=seq(0,1,length.out=ncol(df)), las=2, line=1, font=font.col, cex=cex.col)  
+      if(2 %in% name.sides) mtext(row.labs, side=2, at=seq(0,1,length.out=nrow(df)), las=2, line=1, font=font.row, cex=cex.row)
+      if(3 %in% name.sides) mtext(col.labs, side=3, at=seq(0,1,length.out=ncol(df)), las=2, line=1, font=font.col, cex=cex.col)  
       
       # add a legend for intensity scaling
       if(showLegend) {
@@ -64,7 +65,7 @@ maxScaledHeatmap <- function(df, minMax=0, showMaxScale=FALSE, showLegend=TRUE, 
       #image(t(scale(df, center=F, scale=TRUE)), axes=F, mar=c(10,7),col=colorRampPalette(c("white", "blue"))( 30 ))
       #image(t(df), axes=F, mar=c(10,7),col=colorRampPalette(c("white", "blue"))( 30 ))
       box()
-      grid(nx=ncol(df), ny=nrow(df), col="grey")
+      if(showGrid) grid(nx=ncol(df), ny=nrow(df), col="grey")
       if(4 %in% name.sides)mtext(row.names(df), side=4, at=seq(0,1,length.out=nrow(df)), las=2, line=1)
       if(1 %in% name.sides)mtext(colnames(df), side=1, at=seq(0,1,length.out=ncol(df)), las=2, line=1)  
       if(2 %in% name.sides)mtext(row.names(df), side=2, at=seq(0,1,length.out=nrow(df)), las=2, line=1)
@@ -72,6 +73,14 @@ maxScaledHeatmap <- function(df, minMax=0, showMaxScale=FALSE, showLegend=TRUE, 
       #meta <- paste("TFgrid.R: read counts", normalisation, "minMax", minMax)
       #mtext(meta, side=1, at=1, line=11, las=1, cex=.8)
     } 
+    if(showMaxScale)  {
+      par(xpd=TRUE)
+      maxScaleText <- rep("", length(maxScale.ints))
+      maxScaleText[1] <- "0"
+      maxScaleText[length(maxScale.ints)] <- "Max"
+      legend(1.1, max(y.corners)+y.adjust, legend=rev(maxScaleText), fill=rev(colorRampPalette(c("white", "blue"))( length(maxScale.ints)  )), title="Row scale", xjust=0, yjust=0)
+      par(xpd=FALSE)
+    }
   } else {
     warning("No data to plot") 
   }
@@ -83,5 +92,5 @@ maxScaledHeatmap <- function(df, minMax=0, showMaxScale=FALSE, showLegend=TRUE, 
 #colnames(data.subset) <- sampleLabels[colnames(data.subset)]  # need to convert colnames to standard labels
 
 #maxScaledHeatmap(data.subset, minMax=100)
-#maxScaledHeatmap(data.subset, minMax=100, showMaxScale=TRUE)
+#maxScaledHeatmap(data.subset, minMax=100, , showMaxScale=TRUE,  showIntenseScale=TRUE)
 
