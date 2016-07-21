6c9e3fcfd19eba0a0d6e058c07d286e9f1a589f8
Dave Gerrard <davetgerrard@gmail.com>
2016-07-21 09:43:34 +0100

    tidying moved files

--- R/restore_packages.R
+++
@@ -1,7 +0,0 @@
-# restore_packages.R
-#
-# installs each package from the stored list of packages
-
-load("../Desktop/installed_packages.rda")
-
-for (count in 1:length(installedpackages)) install.packages(installedpackages[count])
