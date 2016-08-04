cafdc1cc6478726e40c3c7d625ed47a8f41a6a10
Dave Gerrard <davetgerrard@gmail.com>
2016-08-04 15:40:32 +0100

    removed dependency on mis-spelled script file

--- R/watermark.R
+++ R/watermark.R
@@ -1,7 +1,7 @@
 
 # prints text faintly in bottom right hand corner. Good with getCurrentScritp.R
 watermark <- function(x, inc.date=T, side=1, line=floor(par()$mar[side]) -1, adj=1, cex=.7, col="lightgrey", ...)  {
-  if(!exists("getCurrentSCript") ) source( "C:/Users/Dave/utilsGerrardDT/getCurrentScript.R")
+  #if(!exists("getCurrentScript") ) source( "getCurrentScript.R")  # not needed now loaded as package 
   if(inc.date) { x <- paste(x, date()) }
   
   mtext(x, side=side, line=line, adj=adj, col=col, cex=cex)
